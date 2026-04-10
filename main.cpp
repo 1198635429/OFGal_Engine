5974eb2b7e00611f6cdcb1b0dddab84552cbc24f
Mr.Seagull <1198635429@qq.com>
2026-04-10 16:45:01 +0800

    已与项目结构预览程序合并，未与事件总线对接

--- main.cpp
+++ main.cpp
@@ -32,12 +32,7 @@ int main() {
 	std::thread inputThread(InputThread, &collector);
 
 	//_EventBus::getInstance().publish_SoundPlay("E:/Projects/C++Projects/OFGal_Engine/acane_madder___Think_of_You.wav");
-
-	pWindowsSystem->OpenProjectStructureViewer("E:\\Projects\\C++Projects\\OFGal_Engine\\x64\\Debug\\ProjectStructureViewer.exe");
-	pWindowsSystem->RefreshProjectStructureViewer();
-	system("pause");
-	pWindowsSystem->CloseProjectStructureViewer();
-
+	
 	//system("pause");
 	running = false;
 	inputThread.join();  // 等待子线程结束
